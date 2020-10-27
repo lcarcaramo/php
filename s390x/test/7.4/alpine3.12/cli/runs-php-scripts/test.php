283fce25b63781abf8751ffd79d3e679db4fba88
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-27 08:55:25 -0400

    Add functional tests

---
+++ s390x/test/7.4/alpine3.12/cli/runs-php-scripts/test.php
@@ -0,0 +1,8 @@
+<?php
+	$ANSI_GREEN = "\e[32m";
+	$ANSI_RESET = "\e[0m";
+
+
+	echo $ANSI_GREEN . "Success! This PHP script is running on the quay.io/ibmz/php image!\n" . $ANSI_RESET;
+	echo $ANSI_GREEN . "Test Complete. Terminating...\n" . $ANSI_RESET;
+?>
--- s390x/test/7.4/alpine3.12/cli/test.sh
+++ s390x/test/7.4/alpine3.12/cli/test.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-echo "No tests yet."
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW It can run a PHP script: $ANSI_RESET"
+docker build -t test/run-script/quay.io/ibmz/php runs-php-scripts
+docker run --rm --name runs-php-scripts test/run-script/quay.io/ibmz/php
+docker rmi test/run-script/quay.io/ibmz/php
+
+echo -e "$ANSI_YELLOW It can install PECL extensions: $ANSI_RESET"
+docker build -t test/pecl/quay.io/ibmz/php can-install-pecl-extensions
+echo -e "\n $ANSI_GREEN Success! Image was built and PECL extensions were installed. $ANSI_RESET \n"
+docker rmi test/pecl/quay.io/ibmz/php
+
+echo -e "$ANSI_YELLOW It can extract and delete docker-php-source: $ANSI_RESET"
+docker build -t test/docker-php-source/quay.io/ibmz/php can-extract-delete-docker-php-source
+echo -e "\n $ANSI_GREEN Success! Extensions provided in docker-php-source were extracted and deleted. $ANSI_RESET \n"
+docker rmi test/docker-php-source/quay.io/ibmz/php
+
+echo -e "$ANSI_YELLOW It can install Core extensions: $ANSI_RESET"
+docker build -t test/core/quay.io/ibmz/php can-install-core-extensions
+echo -e "\n $ANSI_GREEN Success! Image was built and Core extensions were installed. $ANSI_RESET \n"
+docker rmi test/core/quay.io/ibmz/php
+
+echo -e "$ANSI_YELLOW It can install extensions manually: $ANSI_RESET"
+docker build -t test/manual/quay.io/ibmz/php can-manually-install-extensions
+echo -e "\n $ANSI_GREEN Success! Image was built and extensions were installed manually. $ANSI_RESET \n"
+docker rmi test/manual/quay.io/ibmz/php
+
+echo -e "$ANSI_YELLOW It can install extensions manually with helpers: $ANSI_RESET"
+docker build -t test/manual-helpers/quay.io/ibmz/php can-manually-install-extensions-with-helpers
+echo -e "\n $ANSI_GREEN Success! Image was built and extensions were installed manually using helpers. $ANSI_RESET \n"
+docker rmi test/manual-helpers/quay.io/ibmz/php
+
+echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
