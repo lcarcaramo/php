9189e2421a9e64cf5203f1e58bf8f36364de6c99
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-10-27 12:01:46 -0400

    Cleanup test.sh

--- s390x/test/7.4/alpine3.12/cli/test.sh
+++ s390x/test/7.4/alpine3.12/cli/test.sh
@@ -2,40 +2,78 @@
 
 set -e
 
-export ANSI_YELLOW="\e[1;33m"
+export ANSI_YELLOW_BOLD="\e[1;33m"
 export ANSI_GREEN="\e[32m"
+export ANSI_YELLOW_BACKGROUND="\e[1;7;33m"
+export ANSI_GREEN_BACKGROUND="\e[1;7;32m"
+export ANSI_CYAN="\e[36m"
 export ANSI_RESET="\e[0m"
+export DOCKERFILE_TOP="**************************************** DOCKERFILE ******************************************"
+export DOCKERFILE_BOTTOM="**********************************************************************************************"
+export TEST_SUITE_START="**************************************** SMOKE TESTS *****************************************"
+export TEST_SUITE_END="************************************** TEST SUCCESSFUL ***************************************"
 
-echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
+# Pass in path to Dockerfile
+print_dockerfile () {
+        echo -e "\n$ANSI_CYAN$DOCKERFILE_TOP\n$(<$1/Dockerfile)\n$DOCKERFILE_BOTTOM $ANSI_RESET\n"
+}
 
-echo -e "$ANSI_YELLOW It can run a PHP script: $ANSI_RESET"
-docker build -t test/run-script/quay.io/ibmz/php runs-php-scripts
-docker run --rm --name runs-php-scripts test/run-script/quay.io/ibmz/php
-docker rmi test/run-script/quay.io/ibmz/php
+# Pass in test case message
+print_test_case () {
+        echo -e "\n$ANSI_YELLOW_BOLD$1 $ANSI_RESET"
+}
 
-echo -e "$ANSI_YELLOW It can install PECL extensions: $ANSI_RESET"
-docker build -t test/pecl/quay.io/ibmz/php can-install-pecl-extensions
-echo -e "\n $ANSI_GREEN Success! Image was built and PECL extensions were installed. $ANSI_RESET \n"
-docker rmi test/pecl/quay.io/ibmz/php
+print_success () {
+        echo -e "\n $ANSI_GREEN$1 $ANSI_RESET \n"
 
-echo -e "$ANSI_YELLOW It can extract and delete docker-php-source: $ANSI_RESET"
-docker build -t test/docker-php-source/quay.io/ibmz/php can-extract-delete-docker-php-source
-echo -e "\n $ANSI_GREEN Success! Extensions provided in docker-php-source were extracted and deleted. $ANSI_RESET \n"
-docker rmi test/docker-php-source/quay.io/ibmz/php
+}
 
-echo -e "$ANSI_YELLOW It can install Core extensions: $ANSI_RESET"
-docker build -t test/core/quay.io/ibmz/php can-install-core-extensions
-echo -e "\n $ANSI_GREEN Success! Image was built and Core extensions were installed. $ANSI_RESET \n"
-docker rmi test/core/quay.io/ibmz/php
+build () {
+        print_dockerfile $1
+        docker build -q -t $1 $1
+}
 
-echo -e "$ANSI_YELLOW It can install extensions manually: $ANSI_RESET"
-docker build -t test/manual/quay.io/ibmz/php can-manually-install-extensions
-echo -e "\n $ANSI_GREEN Success! Image was built and extensions were installed manually. $ANSI_RESET \n"
-docker rmi test/manual/quay.io/ibmz/php
+cleanup () {
+        docker rmi $1
+}
 
-echo -e "$ANSI_YELLOW It can install extensions manually with helpers: $ANSI_RESET"
-docker build -t test/manual-helpers/quay.io/ibmz/php can-manually-install-extensions-with-helpers
-echo -e "\n $ANSI_GREEN Success! Image was built and extensions were installed manually using helpers. $ANSI_RESET \n"
-docker rmi test/manual-helpers/quay.io/ibmz/php
+suite_start () {
+        echo -e "\n$ANSI_YELLOW_BACKGROUND$TEST_SUITE_START$ANSI_RESET \n"
+}
 
-echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
+suite_end () {
+        echo -e "\n$ANSI_GREEN_BACKGROUND$TEST_SUITE_END$ANSI_RESET \n"
+}
+
+
+suite_start
+        print_test_case "It can run a PHP script:"
+                build "runs-php-scripts"
+                docker run --rm --name runs-php-scripts "runs-php-scripts"
+                cleanup "runs-php-scripts"
+
+        print_test_case "It can install PECL extensions:"
+                build "can-install-pecl-extensions"
+                print_success "Success! Image was built and PECL extensions were installed."
+                cleanup "can-install-pecl-extensions"
+
+        print_test_case "It can extract and delete docker-php-source:"
+                build "can-extract-delete-docker-php-source"
+                print_success "Success! Extensions provided in docker-php-source were extracted and deleted."
+                cleanup "can-extract-delete-docker-php-source"
+
+        print_test_case "It can install Core extensions:"
+                build "can-install-core-extensions"
+                print_success "Success! Image was built and Core extensions were installed."
+                cleanup "can-install-core-extensions"
+
+        print_test_case "It can install extensions manually:"
+                build "can-manually-install-extensions"
+                print_success "Success! Image was built and extensions were installed manually."
+                cleanup "can-manually-install-extensions"
+
+        print_test_case "It can install extensions manually with helpers:"
+                build "can-manually-install-extensions-with-helpers"
+                print_success "Success! Image was built and extensions were installed manually using helpers."
+                cleanup "can-manually-install-extensions-with-helpers"
+suite_end
